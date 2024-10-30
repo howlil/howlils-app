287fa5fd83acd3fd72501e614d91a16e93fdda6f
howlil <muhammadulilabshar27@gmail.com>
2024-10-30 19:50:59 +0700

    update page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,25 +3,26 @@
 import React from "react";
 import Link from "next/link";
 import { navlink } from "@/utils/const";
-import { useIsPathActive } from "@/utils/utils";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  return (
-    <>
-      <div className="text-neutral-400 px-6 py-2 border-neutral-100 bg-neutral-50 bg-opacity-5 backdrop-blur-sm border-opacity-15 border rounded-full space-x-4">
-        {navlink.map((item, i) => {
-          const isActive = useIsPathActive(item.link);
-          return (
-            <Link
-              key={i}
-              href={item.link}
-              className={isActive ? "text-primary border-b pb-3 gradient-border" : ""}
-            >
-              {item.name}
-            </Link>
-          );
-        })}
-      </div>
-    </>
+  const currentPath = usePathname();
+
+  return (  
+    <div className="text-neutral-400 px-6 py-2 border-neutral-100 bg-neutral-50 bg-opacity-5 backdrop-blur-sm border-opacity-15 border rounded-full space-x-4">
+      {navlink.map((item, i) => {
+        const isActive = currentPath === item.link;
+
+        return (
+          <Link
+            key={i}
+            href={item.link}
+            className={isActive ? "text-primary border-b pb-3 gradient-border" : ""}
+          >
+            {item.name}
+          </Link>
+        );
+      })}
+    </div>
   );
 }
--- src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-'use client';
-import { usePathname } from 'next/navigation';
-
-export function useIsPathActive(targetPath: string): boolean {
-    const currentPath = usePathname();
-    return currentPath === targetPath;
-}
